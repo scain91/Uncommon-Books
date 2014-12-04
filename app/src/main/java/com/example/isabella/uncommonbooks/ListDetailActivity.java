31628bdfd271b6defc5b0799ccd559b39ae70cab
Sara Cain <scain91>
2014-12-03 23:29:02 -0600

    added menu options to all activity views

--- app/src/main/java/com/example/isabella/uncommonbooks/ListDetailActivity.java
+++ app/src/main/java/com/example/isabella/uncommonbooks/ListDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.isabella.uncommonbooks;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -50,6 +52,30 @@ public class ListDetailActivity extends Activity{
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_results, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if(id == R.id.view_lists){
+            Intent intent = new Intent(this, ListOfListsActivity.class);
+            startActivity(intent);
+            System.out.println("Got past startActivity");
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
--- app/src/main/java/com/example/isabella/uncommonbooks/ListOfListsActivity.java
+++ app/src/main/java/com/example/isabella/uncommonbooks/ListOfListsActivity.java
@@ -90,4 +90,29 @@ public class ListOfListsActivity extends Activity {
         super.onStop();
         active = false;
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_results, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if(id == R.id.view_lists){
+            Intent intent = new Intent(this, ListOfListsActivity.class);
+            startActivity(intent);
+            System.out.println("Got past startActivity");
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
